0847e57fb8738c1db150094418d02f52b0a7f8da
CaglaOkmen <126767927+CaglaOkmen@users.noreply.github.com>
2024-09-17 08:31:28 +0300

    -

--- bir_ws/bir_ws/build/turtlesim_interfaces/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/cagla/bir_ws/src/turtlesim_interfaces
-# Build directory: /home/cagla/bir_ws/build/turtlesim_interfaces
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("turtlesim_interfaces__py")
